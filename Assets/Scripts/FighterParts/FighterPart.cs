e3292820dbe449f7f300987fd7236c8a3d7f6d16
Arjen van Klaveren <arjenvklaveren@gmail.com>
2022-05-12 12:33:13 +0200

    Weapon Collision Fix + working hammer again

--- Assets/Scripts/FighterParts/FighterPart.cs
+++ Assets/Scripts/FighterParts/FighterPart.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Lean.Pool;
+using DG.Tweening;
+using TMPro;
 
 public class FighterPart : MonoBehaviour
 {
     public float healthPoints;
     public float weight;
-    public void TakeDamage(int damage)
+
+    [SerializeField] protected GameObject damageText;
+ 
+    public void TakeDamage(int damage, Vector3 hitPos)
     {
         healthPoints -= damage;
+        GameObject damageTextObject = LeanPool.Spawn(damageText, hitPos, transform.rotation);
+        damageTextObject.GetComponent<TMPro.TextMeshPro>().alpha = 1;
+        damageTextObject.GetComponent<TMPro.TextMeshPro>().text = damage.ToString();
+        damageTextObject.transform.DOMoveY(transform.position.y + damageTextObject.transform.position.y + 2, 3);
+        LeanPool.Despawn(damageTextObject, 3);
     }
 }
--- Assets/Scripts/FighterParts/FighterWeapon/Weapons/Hammer.cs
+++ Assets/Scripts/FighterParts/FighterWeapon/Weapons/Hammer.cs
@@ -1,22 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 using DG.Tweening;
 
 public class Hammer : FighterWeapon, IWeapon
 {
     [SerializeField] float hammerForce;
+    [SerializeField] float hammerLaunchForceMultiplier;
     [SerializeField] GameObject hammerTip;
     [SerializeField] LayerMask hammerTipMask;
 
     bool isSwinging;
 
+    Coroutine hammerSwingRotationRoutine;
+
     public override void ActivateWeapon()
     {
         if (transform.localEulerAngles.x > -1 && transform.localEulerAngles.x < 1)
         {
-            float hammerTime = 100 / hammerForce;
-            transform.DOLocalRotate(new Vector3(100, 0, 0), hammerTime, RotateMode.Fast);
+            float hammerTime = (100 / hammerForce) / 2;
+            hammerSwingRotationRoutine = RotateObject.instance.RotateObjectToAngle(this.transform.gameObject, new Vector3(90, 0, 0), hammerTime);
             isSwinging = true;
             StartCoroutine(ResetHammerWhenDone(hammerTime));
         }
@@ -24,35 +28,53 @@ public class Hammer : FighterWeapon, IWeapon
 
     public void Update()
     {
-        if(isSwinging) CheckCollision();
+        if (isSwinging) CheckCollision();
     }
 
     public override void CheckCollision()
     {
-        Collider[] hits = Physics.OverlapBox(hammerTip.transform.position, hammerTip.transform.localScale / 2, hammerTip.transform.rotation);
-        if (hits.Length > 0)
-        {
-            //Debug.Log("Hit something");
+        List<Collider> tipHits = new List<Collider>();
+        List<Collider> hits = new List<Collider>();
 
+        foreach (Collider childCollider in GetComponentsInChildren<Collider>())
+        {
+            if (childCollider.gameObject != hammerTip.gameObject)
+            {
+                hits.AddRange(Physics.OverlapBox(childCollider.transform.position, childCollider.transform.lossyScale / 2, childCollider.transform.rotation, Physics.AllLayers, QueryTriggerInteraction.Collide).ToList());
+            }
+        }
+        tipHits.AddRange(Physics.OverlapBox(hammerTip.transform.position, hammerTip.transform.lossyScale / 2, hammerTip.transform.rotation, Physics.AllLayers, QueryTriggerInteraction.Collide).ToList());
+
+        if (hits.Count > 0)
+        {
+            //TODO only add alot of force when hammertip or maybe even fix for stick
             foreach (Collider hit in hits)
             {
                 if (hit.gameObject.transform.root != this.gameObject.transform.root)
                 {
-                    transform.DOKill();
-                    transform.DOLocalRotate(new Vector3(0, 0, 0), 100 / (hammerForce / 4), RotateMode.Fast);
-                    isSwinging = false;
+                    //Debug.Log("Hit something " + hit.name);
 
-                    if (hit.gameObject.transform.root.CompareTag("Fighter"))
-                    {
-                        Fighter otherFighter = hit.gameObject.transform.root.GetComponent<Fighter>();
-                        otherFighter.GetComponent<Rigidbody>().AddForceAtPosition(hit.transform.InverseTransformPoint(hammerTip.transform.position), hammerTip.transform.forward * 56000);
-                    }
+                    RotateObject.instance.StopCoroutine(hammerSwingRotationRoutine);
+                    isSwinging = false;
+                    hammerSwingRotationRoutine = RotateObject.instance.RotateObjectToAngle(this.transform.gameObject, new Vector3(0, 0, 0), transform.localEulerAngles.x / 90);
+                }
+            }
+        }
+
+        if (tipHits.Count > 0)
+        {
+            foreach (Collider hit in tipHits)
+            {
+                if (hit.gameObject.transform.root != this.gameObject.transform.root && hit.gameObject.transform.root.CompareTag("Fighter"))
+                {
+                    Fighter otherFighter = hit.gameObject.transform.root.GetComponent<Fighter>();
+                    otherFighter.GetComponent<Rigidbody>().AddForceAtPosition(hit.transform.InverseTransformPoint(hammerTip.transform.position), hammerTip.transform.forward * ((hammerForce * hammerLaunchForceMultiplier) * transform.localEulerAngles.x));
+
                     if (hit.GetComponentInParent<FighterPart>())
                     {
                         FighterPart part = hit.GetComponentInParent<FighterPart>();
-                        part.TakeDamage(damage);
-                        Debug.Log("hit part: " + part.name);
-                    }
+                        part.TakeDamage(damage, hammerTip.transform.position);
+                    }             
                 }
             }
         }
@@ -61,7 +83,7 @@ public class Hammer : FighterWeapon, IWeapon
     private IEnumerator ResetHammerWhenDone(float hammerTime)
     {
         yield return new WaitForSeconds(hammerTime);
-        //transform.DOLocalRotate(new Vector3(0, 0, 0), 100 / (hammerForce / 4), RotateMode.Fast);
         isSwinging = false;
+        hammerSwingRotationRoutine = RotateObject.instance.RotateObjectToAngle(this.transform.gameObject, new Vector3(0, 0, 0), transform.localEulerAngles.x / 90);
     }
 }
