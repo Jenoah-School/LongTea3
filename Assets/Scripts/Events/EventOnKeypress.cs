36b5a556f0c41d44125b70aa07fedef9418dc6ce
Jenoah Kers <hello@jenoah.nl>
2022-05-12 21:46:54 +0200

    Changed wheel mass

--- Assets/Scripts/Events/EventOnKeypress.cs
+++ Assets/Scripts/Events/EventOnKeypress.cs
@@ -8,14 +8,23 @@ public class EventOnKeypress : MonoBehaviour
 {
     [SerializeField] private List<InputActionReference> keys = new List<InputActionReference>();
     [SerializeField] private UnityEvent OnKeyPress;
+    private PlayerInput playerInput;
+    private PlayerControls playerControls;
+
+    private void Start()
+    {
+        playerControls = new PlayerControls();
+        playerInput = FindObjectOfType<PlayerInput>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         foreach(InputActionReference key in keys)
         {
-            if (key.action.WasPerformedThisFrame())
+            if(playerControls.Regular.PrimaryWeapon.WasPerformedThisFrame())
             {
+                Debug.Log("YEEEAA");
                 OnKeyPress.Invoke();
             }
         }
