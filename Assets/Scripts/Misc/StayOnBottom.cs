eca8dc4aa96bf6b43347198722253d6c8ba1ba83
Jenoah Kers <hello@jenoah.nl>
2022-06-10 12:33:31 +0200

    Fixed ring not moving, added game scene, bloom and antenna

--- Assets/Scripts/Misc/StayOnBottom.cs
+++ Assets/Scripts/Misc/StayOnBottom.cs
@@ -9,6 +9,7 @@ public class StayOnBottom : MonoBehaviour
     [SerializeField] private float maxDistance = 25f;
     [SerializeField] private Vector3 offset = new Vector3(0f, 0.05f, 0f);
     [SerializeField] private Transform castOrigin = null;
+    [SerializeField] private Vector3 castOffset = new Vector3(0f, 0.1f, 0f);
 
     private Vector3 targetPosition = Vector3.zero;
     private Quaternion targetRotation = Quaternion.identity;
@@ -16,7 +17,7 @@ public class StayOnBottom : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(castOrigin && Physics.Raycast(castOrigin.position, direction, out RaycastHit hit, maxDistance, hitLayers))
+        if(castOrigin && Physics.Raycast(castOrigin.position + castOffset, direction, out RaycastHit hit, maxDistance, hitLayers))
         {
             targetRotation = Quaternion.LookRotation(hit.normal);
             targetPosition = hit.point;
