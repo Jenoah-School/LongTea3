85de49e8e7c40e6453a2155685df790849fabe9f
Arjen van Klaveren <arjenvklaveren@gmail.com>
2022-06-20 21:58:25 +0200

    Sawblade

--- Assets/Scripts/Creators/FighterCreator.cs
+++ Assets/Scripts/Creators/FighterCreator.cs
@@ -21,7 +21,7 @@ public class FighterCreator : MonoBehaviour
         Fighter fighterDummy;
         if (spawnOnInitialisation)
         {
-            fighter = CreateNewFighter(0, 6, 7, 4);
+            fighter = CreateNewFighter(0, 2, 7, 5);
             fighter.transform.name = "main";
         }
         if (spawnDummyOnInitialisation)
--- Assets/Scripts/FighterParts/FighterWeapon/Weapons/Sawblade.cs
+++ Assets/Scripts/FighterParts/FighterWeapon/Weapons/Sawblade.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using DG.Tweening;
 
 public class Sawblade : FighterWeapon
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject sawBladeObject;
+    [SerializeField] float cooldown;
+    [SerializeField] float extendTime;
+
+    float prevDamageTime;
+    float prevExtendTime;
+
+    public override void ActivateWeapon(InputAction.CallbackContext context)
+    {
+        if (context.action.WasPerformedThisFrame())
+        {
+            if (Time.time > prevExtendTime)
+            {
+                prevExtendTime = Time.time + cooldown;
+                StartCoroutine(ExtendWeapon());
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.transform.GetComponentInParent<Fighter>() && Time.time >= prevDamageTime && other.transform.gameObject.layer != LayerMask.NameToLayer("Ignore Raycast"))
+        {
+            prevDamageTime = Time.time + 0.1f;
+            Fighter hitFighter = other.transform.GetComponentInParent<Fighter>();
+            hitFighter.TakeDamage(damage, fighterRoot, true, true);
+        }
+    }
+
+    private IEnumerator ExtendWeapon()
+    {
+        float initialZPos = sawBladeObject.transform.localPosition.z;
+        sawBladeObject.transform.DOLocalMoveZ(2, extendTime/3);
+        yield return new WaitForSeconds(extendTime);
+        sawBladeObject.transform.DOLocalMoveZ(initialZPos, extendTime/3);
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        sawBladeObject.transform.Rotate(0, 0, 1000 * Time.deltaTime);
     }
 }
